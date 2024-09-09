3de9ca197ff175867d7ab296b31f6c3237084837
DahianaDuque <dahiana.duque567@pascualbravo.edu.co>
2024-09-09 17:15:54 -0500

    Se realiza las operaciones con variables y el operador ternario

--- index.js
+++ index.js
@@ -21,4 +21,24 @@ const SegundosMinuto= 60;
 
 //Operaciones con las variables
 
+let MasaCorporal = Peso/(Altura*Altura)
+console.log("El Imc de " + Nombre+" es " + MasaCorporal)
 
+let Diasvividos = Edad * (365 * Meses /12);
+console.log(Nombre+" Ha vivido "+Diasvividos+ "dias")
+
+let minutosmes = 30*HorasDia*MinutosHora;
+console.log ("Un mes tiene "+ minutosmes +" minutos")
+
+
+
+//Operador  Ternario
+ 
+let Mayoredad = Edad>= 18 ? "Es mayor de edad": "No es mayor de edad";
+console.log("La persona "+ Nombre + "es :" +Mayoredad )
+
+let Esfan = CancionFavorita === "Nosense-Sabrina" ? "Es su cancion favorita": "No es su cancion favorita"
+console.log(Nombre+"tiene de cancion favorita"+Esfan)
+
+let Tienehermanos= Numerodehermanos >= 2 ? "Tiene varios hermanos ": "Tiene uno o ningun hermano"
+console.log(Nombre+"Tiene "+Tienehermanos)
