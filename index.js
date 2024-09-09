e53993ab7c0dd2880d7c7a09e40d8fce345c8016
DahianaDuque <dahiana.duque567@pascualbravo.edu.co>
2024-09-09 16:55:05 -0500

    Creacion de las variables

---
+++ index.js
@@ -0,0 +1,24 @@
+ // 10 Variables let 
+ 
+ let Nombre = "Maria";
+ let Peso = 58;
+ let Edad = "23";
+ let Ciudad = "Barranquilla";
+ let Colorfavorito = "Rosa";
+ let MateriaFavorita = "Ingles";
+ let CancionFavorita = "Nosensen-Sabrina";
+ let Numerodehermanos= "3";
+ let Altura = 1.64;
+ let ComidaFavorita = "Sushi";
+ 
+ // 5 Variables const
+
+const DiaSemana= 7;
+const Meses= 12;
+const HorasDia= 24;
+const MinutosHora= 60;
+const SegundosMinuto= 60;
+
+//Operaciones con las variables
+
+
